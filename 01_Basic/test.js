e6052a9927ace8c432af00a9d5ef1bd7d2c314dd
sourav raj <sourav2001raj@gmail.com>
2024-07-02 06:09:49 +0000

    number system

--- 01_Basic/test.js
+++ 01_Basic/test.js
@@ -5,4 +5,11 @@ let age  = 26
 age = 32
 name = "Sumit Anand"
 // empId = 33333
-console.table([empId, name, age])
+// console.table([empId, name, age])
+// console.log(typeof name);
+// console.log( "2" + "3");
+console.log(Math.random()*10)
+console.log(Math.floor(Math.random()* 10 + 1) );
+let min =10;
+let max  = 20;
+console.log(Math.floor(Math.random()* (max - min +1 )) + min);
